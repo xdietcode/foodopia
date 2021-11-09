442734875b80f80563373b3791d2b4b11bf987c3
xdietcode <xdietcode@gmail.com>
2021-11-09 12:49:41 -0800

    create controllers for Cart, Checkout, and ItemOrder

---
+++ src/main/java/com/xdietcode/foodopia/controller/ItemOrderController.java
@@ -0,0 +1,19 @@
+package com.xdietcode.foodopia.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class ItemOrderController {
+
+    @RequestMapping(value = "/order/{menuId}", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void addMenuItemToCart(@PathVariable("menuId") int menuId) {
+        // implement adding menuId to cart
+
+    }
+}
