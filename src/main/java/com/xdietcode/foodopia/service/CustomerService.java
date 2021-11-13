e6e8fb3386b4567d92f7a6215907f8be9335759c
xdietcode <xdietcode@gmail.com>
2021-11-13 01:05:51 -0800

    implement signUp and getCustomer for customerService

--- src/main/java/com/xdietcode/foodopia/service/CustomerService.java
+++ src/main/java/com/xdietcode/foodopia/service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.xdietcode.foodopia.service;
 
 import com.xdietcode.foodopia.dao.CustomerDao;
+import com.xdietcode.foodopia.entity.Cart;
 import com.xdietcode.foodopia.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,22 @@ public class CustomerService {
     @Autowired // inject CustomerDao into Customer Service
     private CustomerDao customerDao;
 
+    /**
+     * @param customer
+     */
     public void signUp(Customer customer) {
+        // for each customer, create a cart
+        Cart cart = new Cart();
+        customer.setCart(cart);
+        customer.setEnabled(true);
+
+        customerDao.signUp(customer);
     }
 
+    /**
+     * @param email
+     * @return customer
+     */
     public Customer getCustomer(String email) {
         return customerDao.getCustomer(email);
     }
